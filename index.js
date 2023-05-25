714a883499c9d7b5b0518249bae642be4e400d1c
Srinivas <msrinivas0025@gmail.com>
2023-05-25 12:08:53 +0530

    3rd commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const flash = require('connect-flash');
